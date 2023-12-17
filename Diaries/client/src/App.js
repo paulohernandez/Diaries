0364dc0c673bfe285e175ca038b5120a04314517
Paulo13 <107038071+Tomador13@users.noreply.github.com>
2023-12-17 23:06:18 +0800

    fiiixx

--- Diaries/client/src/App.js
+++ Diaries/client/src/App.js
@@ -17,12 +17,13 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Diaries</h1>
+      <h3>Unknown thoughts by someone</h3>
       <div className="status-container">
         <div className="status-information">
           <h1>
-            This is the founder of diaries , Hope that you are doing great 1,
-            grinding your self to become WHAT YOU WANT IN LIFE!
+            This is the founder of diaries , Hope that you are doing great !,
+            grinding yourself to become WHAT YOU WANT IN LIFE!
           </h1>
         </div>
         <div className="status-react-container">
