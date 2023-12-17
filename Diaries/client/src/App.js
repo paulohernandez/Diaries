d3028191070e34fbcb4d618445006eb42bc36ef8
Paulo13 <107038071+Tomador13@users.noreply.github.com>
2023-12-17 22:49:43 +0800

    feat added first note

--- Diaries/client/src/App.js
+++ Diaries/client/src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ like , setLike] = useState(0)
+  const [isLike , setIsLike] = useState(false)
+
+  const handleClick = () => {
+
+    if(!isLike){
+      setLike(1)
+      setIsLike(true)
+    }else{
+      setLike(0)
+      setIsLike(false)
+    }
+
+    
+  }
+
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <div className='status-container'>
+        <div className='status-information'>
+          <h1>This is the founder of diaries , Hope that you are doing great , grinding your self to become WHAT YOU WANT IN LIFE!</h1>
+        </div>
+        <div className='status-react-container'>
+          <h2>{like}</h2>
+          <button onClick={handleClick} >Like</button>
+        </div>
+      </div>
+
     </div>
   );
 }
