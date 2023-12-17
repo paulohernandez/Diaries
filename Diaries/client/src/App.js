754046ab21f26e5abd09f9a3c9b9556903702d6e
Paulo13 <107038071+Tomador13@users.noreply.github.com>
2023-12-17 23:09:19 +0800

    hereee

--- Diaries/client/src/App.js
+++ Diaries/client/src/App.js
@@ -1,36 +1,19 @@
 import { useState } from "react";
 import "./App.css";
+import Status from "./components/Status";
 
 function App() {
-  const [like, setLike] = useState(0);
-  const [isLike, setIsLike] = useState(false);
 
-  const handleClick = () => {
-    if (!isLike) {
-      setLike(1);
-      setIsLike(true);
-    } else {
-      setLike(0);
-      setIsLike(false);
-    }
-  };
 
   return (
     <div className="App">
       <h1>Diaries</h1>
       <h3>Unknown thoughts by someone</h3>
-      <div className="status-container">
-        <div className="status-information">
-          <h1>
-            This is the founder of diaries , Hope that you are doing great !,
-            grinding yourself to become WHAT YOU WANT IN LIFE!
-          </h1>
-        </div>
-        <div className="status-react-container">
-          <h2>{like}</h2>
-          <button onClick={handleClick}>Like</button>
-        </div>
-      </div>
+      
+    <Status/>
+    <Status/>
+    <Status/>
+    <Status/>
     </div>
   );
 }
---
+++ Diaries/client/src/components/Status.jsx
@@ -0,0 +1,30 @@
+import {useState} from 'react'
+
+export default function Status() {
+    const [like, setLike] = useState(0);
+    const [isLike, setIsLike] = useState(false);
+  
+    const handleClick = () => {
+      if (!isLike) {
+        setLike(1);
+        setIsLike(true);
+      } else {
+        setLike(0);
+        setIsLike(false);
+      }
+    };
+  return (
+    <div className="status-container">
+    <div className="status-information">
+      <h1>
+        This is the founder of diaries , Hope that you are doing great !,
+        grinding yourself to become WHAT YOU WANT IN LIFE!
+      </h1>
+    </div>
+    <div className="status-react-container">
+      <h2>{like}</h2>
+      <button onClick={handleClick}>Like</button>
+    </div>
+  </div>
+  )
+}
