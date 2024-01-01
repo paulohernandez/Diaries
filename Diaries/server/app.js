fa5b71dcd649dd0f2696fc70863e0a6f0d71c806
Paulo13 <107038071+Tomador13@users.noreply.github.com>
2024-01-01 17:34:38 +0800

    feat added

---
+++ Diaries/server/app.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const PORT = process.env.PORT
+const express = require('express')
+
+const app = express()
+
+
+app.listen(PORT , () => {
+    console.log(`listening to port ${PORT}`);
+})
